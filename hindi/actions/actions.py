6ad91d51e1ca02370a6edb9723b80510dd6c0bd7
Prakash <prakashram1327@gmail.com>
2023-07-30 11:49:09 +0530

    progress

--- hindi/actions/actions.py
+++ hindi/actions/actions.py
@@ -21,7 +21,79 @@
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
 #         dispatcher.utter_message(text="Hello World!")
-#
 #         return []
+# from typing import Any, Dict, List, Text
+# from iso639 import languages
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.events import SlotSet
+# from rasa_sdk.executor import CollectingDispatcher
+# from textblob import TextBlob
+# class ActionDetectLanguage(Action):
+#     def name(self) -> Text:
+#         return "action_detect_language"
+
+#     def run(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any],
+#     ) -> List[Dict[Text, Any]]:
+
+#         text = tracker.latest_message.get("text")
+#         langcode = TextBlob(text).detect_language()
+
+#         langname = languages.get(alpha2=langcode).name
+#         langname = langname if "(" not in langname else langname.split(" ")[0]
+
+#         return [SlotSet("langcode", langcode), SlotSet("langname", langname)]
+
+# actions.py
+from typing import Any, Dict, List, Text
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from langdetect import detect
+
+# class ActionDetectLanguage(Action):
+#     def name(self) -> Text:
+#         return "action_detect_language"
+
+#     def run(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any],
+#     ) -> List[Dict[Text, Any]]:
+
+#         text = tracker.latest_message.get("text")
+#         langcode = detect(text)
+
+#         return [SlotSet("langcode", langcode)]
+
+import spacy
+
+class ActionDetectLanguage(Action):
+    def __init__(self):
+        self.nlp = spacy.load("xx_ent_wiki_sm")  # Load multilingual language model
+
+    def name(self) -> Text:
+        return "action_detect_language"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        text = tracker.latest_message.get("text")
+        langcode = self.detect_language(text)
+
+        return [SlotSet("langcode", langcode)]
+
+    def detect_language(self, text):
+        doc = self.nlp(text)
+        langcode = doc._.language["language"]
+        return langcode
+
